de452b7c193f8c48bdd69679ee8b5c2571b06fb5
hieuvu99 <star.hieu.99@gmail.com>
2020-11-20 12:28:48 +0700

    create layout for register and login

---
+++ app/src/main/java/eu/berdosi/app/heartbeat/LoginActivity.java
@@ -0,0 +1,34 @@
+package eu.berdosi.app.heartbeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.Navigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import eu.berdosi.app.heartbeat.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+    private String inputEmail;
+    private String inputPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+
+        binding.buttonRegister.setOnClickListener(view -> {
+            startActivity(new Intent(this,RegisterActivity.class));
+        });
+
+        binding.buttonLogin.setOnClickListener(view -> {
+            inputEmail = binding.editTextEmail.getText().toString();
+            inputPassword = binding.editTextPassword.getText().toString();
+            Toast.makeText(getApplicationContext(), inputEmail+ "and" + inputPassword,
+                Toast.LENGTH_LONG).show();
+        });
+    }
+}
